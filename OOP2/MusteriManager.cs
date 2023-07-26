5225e16ad134501de6dc074efa4e892997a3f739
Rugeyye <rugozdemr@gmail.com>
2023-07-26 13:47:57 +0300

    First Commit

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP2
+{
+    internal class MusteriManager
+    {
+        internal void Ekle(GercekMusteri musteri1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
